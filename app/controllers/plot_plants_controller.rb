92107c6fd0d7c1e8261c97dd27e09f0c6db628d7
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-25 11:26:01 -0600

    feat: finish user story 2

--- app/controllers/plot_plants_controller.rb
+++ app/controllers/plot_plants_controller.rb
@@ -1,2 +1,9 @@
 class PlotPlantsController < ApplicationController
+  def destroy
+    @plot = Plot.find(params[:plot_id])
+    @plant = Plant.find(params[:id])
+    @plot_plant = PlotPlant.find_by(plant_id: @plant.id, plot_id: @plot.id)
+    @plot_plant.destroy
+    redirect_to plots_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :plot_plants
+  resources :plot_plants, only: [:destroy]
   resources :plants
   resources :plots, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
